b3cd3d1536bd548dd0afeff0e46e1004815bbda7
lollozip <lorenzo.orsingher@gmail.com>
2023-07-14 20:29:59 +0200

    add MSE and adapt to new denorm

--- train.py
+++ train.py
@@ -4,14 +4,17 @@ from copy import copy
 
 import torch
 from torch.utils.data import DataLoader
-from torch.optim import SGD
+from torch.optim import SGD, Adam
+from torch.nn import MSELoss
+
 import numpy as np
 import cv2 as cv
 import lpips
 
 from model_files.resunet import ResNetUNet
 from dataLoader import CustomDataset
-from utils import parse_argv
+
+from utils import parse_argv, save_loss
 
 
 
@@ -54,10 +57,13 @@ print(model)
 
 #setup optimizer and loss function
 opt = SGD(model.parameters(), lr=LR)
+#opt = Adam(model_parameters, lr=1e-3, betas=(0.9, 0.999))
 lpips_loss = lpips.LPIPS(net='alex')
 if DEVICE == "cuda":
   lpips_loss.cuda()
 lossFunc = lpips_loss
+
+lossFunc2 = MSELoss()
 
 epoch = 0
 
@@ -99,48 +105,63 @@ for i in range(epoch, EPOCH):
     for batch_id, (X,y) in enumerate(data_loader):
 
         #convert tensors to float and load em to device
-        X = X.float()
-        y = y.float()
+        # X = X.float()
+        # y = y.float()
         (X,y) = (X.to(DEVICE), y.to(DEVICE))
 
         #actual trainign lol #####
         predictions = model(X)
+        #breakpoint()
+        # _, denom_y = dataset.denormalize_loss(None, y)
+        # _, denom_pred = dataset.denormalize_loss(None, predictions)
+        # denom_y = denom_y[0].permute(1,2,0).cpu().numpy()
+        # denom_x = denom_x[0].permute(1,2,0).cpu().numpy()
+        # denom_y = dataset.tensor_as_img(denom_y)
+        # denom_x = dataset.tensor_as_img(denom_x)
+        # ynew = dataset.invTransformCol(y)[0].permute(1,2,0).numpy()
+        # Xnew = dataset.invTransformBw(X)[0].permute(1,2,0).numpy()
+        xnew = dataset.UnBw(X)[0].permute(1,2,0).numpy()
+        ynew = dataset.UnCol(y)[0].permute(1,2,0).numpy()
 
-        loss = lossFunc(predictions, y)
+        # print(ynew.min(), "   ", ynew.min())
+        # print(ynew.max(), "   ", ynew.max())
+        #breakpoint()
+        # cv.imshow("im",denom_y)
+
+        # cv.imshow("im2",np.hstack([dataset.tensor_as_img(ynew),cv.cvtColor(dataset.tensor_as_img(xnew),cv.COLOR_GRAY2BGR)]) )
+        # cv.waitKey(1)
+
+        #breakpoint()
+
+        loss1 = lossFunc(predictions, y).mean()
+        loss2 = lossFunc2(predictions, y)
+        #breakpoint()
+        print(loss1, " ", loss2)
+        loss = loss1 + loss2
 
         opt.zero_grad()
         #breakpoint()
-        loss.sum().backward()
+        loss.backward()
         opt.step()
         ##########################
 
         count+=1
         if VIS_DEBUG != False and count%VIS_DEBUG==0 :
-
-            #basically multiply std and add mean for each channel
-            Ximg, _ = dataset.denormalize(copy(X[0].detach().transpose(0,2).cpu().numpy()),None)
-            pred, yimg = dataset.denormalize(copy(predictions[0].detach().transpose(0,2).cpu().numpy()),copy(y[0].detach().transpose(0,2).cpu().numpy()))
-
-            #tensor to ndarray, resize and gray to bgr to allaw hstacking
-            Ximg = Ximg.astype(np.uint8)
-            Ximg = cv.resize(Ximg, yimg.shape[:2])
-            Ximg = cv.cvtColor(Ximg,cv.COLOR_GRAY2BGR)
-            yimg = yimg.astype(np.uint8)
-            #some values exceed 254 or are negative (no tanh, sigmoid or similar in net
-            #because data is already standardized)
-            pred[pred > 254] = 254
-            pred[pred < 0] = 0
-            pred = pred.astype(np.uint8)
+            
+            Ximg = cv.cvtColor(dataset.tensor_as_img(dataset.UnBw(X)[0].permute(1,2,0).detach().cpu().numpy()), cv.COLOR_GRAY2BGR)
+            yimg = dataset.tensor_as_img(dataset.UnCol(y)[0].permute(1,2,0).detach().cpu().numpy())
+            pred = dataset.tensor_as_img(dataset.UnCol(predictions)[0].permute(1,2,0).detach().cpu().numpy())
 
             cv.imshow("im",np.hstack([Ximg,yimg,pred]))
             cv.imwrite(img_savepath+"img_"+str(i)+"_"+str(count)+".jpg", np.hstack([Ximg,yimg,pred]))
-            print("batch_loss: ", round(loss.sum().item()/BATCH,4))
+            print("batch_loss: ", round(loss.item(),4))
             cv.waitKey(1)
 
-            loss_dict["epoch_"+str(i)]["batch_"+str(count/VIS_DEBUG)] = round(loss.sum().item()/BATCH,5)
+            loss_dict["epoch_"+str(i)]["batch_"+str(count/VIS_DEBUG)] = round(loss.item(),5)
             save_loss(loss_dict, current_savepath)
             #breakpoint()
-        epochLoss += loss.sum().item()
+        #breakpoint()    
+        epochLoss += loss.item()
         batchItems += BATCH
     epoch_end = time.time()
     epoch_time = epoch_end - epoch_start
