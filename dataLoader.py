0a7557d717646fbbabac0f3b3ec69c7795e2b3b1
lollozip <lorenzo.orsingher@gmail.com>
2023-07-14 20:29:45 +0200

    switch to transform for norm/denorm

--- dataLoader.py
+++ dataLoader.py
@@ -6,6 +6,58 @@ from torch.utils.data import Dataset, DataLoader
 import os
 import json 
 import cv2 as cv
+import torchvision  
+
+# class UnNormalize(object):
+#     def __init__(self, mean, std):
+#         self.mean = np.array(mean, dtype=np.float32)
+#         self.std = np.array(std, dtype=np.float32)
+
+#     def __call__(self, tensor):
+#         """
+#         Args:
+#             tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+#         Returns:
+#             Tensor: Normalized image.
+#         """
+#         t_clone = tensor.clone()
+#         for t, m, s in zip(t_clone, self.mean, self.std):
+#             t.mul_(s).add_(m)
+#             # The normalize code -> t.sub_(m).div_(s)
+#         return t_clone
+
+# class Normalize(object):
+#     def __init__(self, mean, std):
+#         self.mean = np.array(mean, dtype=np.float32)
+#         self.std = np.array(std, dtype=np.float32)
+
+#     def __call__(self, tensor):
+#         """
+#         Args:
+#             tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+#         Returns:
+#             Tensor: Normalized image.
+#         """
+#         t_clone = tensor.clone()
+#         for t, m, s in zip(t_clone, self.mean, self.std):
+#             t.sub_(m).div_(s)
+#             # The normalize code -> t.sub_(m).div_(s)
+#         return t_clone
+
+class NormalizeInverse(torchvision.transforms.Normalize):
+    """
+    Undoes the normalization and returns the reconstructed images in the input domain.
+    """
+
+    def __init__(self, mean, std):
+        mean = torch.as_tensor(np.array(mean, dtype=np.float32))
+        std = torch.as_tensor(np.array(std, dtype=np.float32))
+        std_inv = 1 / (std + 1e-7)
+        mean_inv = -mean * std_inv
+        super().__init__(mean=mean_inv, std=std_inv)
+
+    def __call__(self, tensor):
+        return super().__call__(tensor.clone())
 
 class CustomDataset(Dataset):
     def __init__(self, insize, outsize, datapath, jsonpath):
@@ -31,7 +83,49 @@ class CustomDataset(Dataset):
         self.insize = insize
         self.outsize = outsize
 
+
+        self.meanC = [  self.jsondata["mean"]["vmc2"],
+                        self.jsondata["mean"]["vmc1"],
+                        self.jsondata["mean"]["vmc0"]]
+        
+        self.stdC = [   self.jsondata["std"]["stdc2"],
+                        self.jsondata["std"]["stdc1"],
+                        self.jsondata["std"]["stdc0"]]
+        
+        self.meanB = [self.jsondata["mean"]["vmbw"]]
+
+        self.stdB = [self.jsondata["std"]["stdbw"]]
+
+        self.NCol = torchvision.transforms.Compose([
+                    torchvision.transforms.Normalize(
+                        mean=self.meanC,
+                        std=self.stdC,
+                    ),
+                ])
+        
+        self.UnCol = NormalizeInverse(
+                        mean=self.meanC,
+                        std=self.stdC,
+                    )
+        
+        self.NBw = torchvision.transforms.Compose([
+            torchvision.transforms.Normalize(
+                mean=self.meanB,
+                std=self.stdB,
+            ),
+        ])
+
+        self.UnBw = NormalizeInverse(
+                        mean=self.meanB,
+                        std=self.stdB,
+                    )
+
+        
     def normalize(self, Ximg, yimg):
+        # from copy import copy
+        # import torchvision
+
+        # xi = copy(Ximg)
 
         yimg = yimg.astype(np.float64)
         
@@ -70,6 +164,33 @@ class CustomDataset(Dataset):
 
         return Ximg, yimg
 
+    def denormalize_loss(self, Ximg, yimg):
+
+        if not Ximg is None:
+            Ximg = Ximg * self.jsondata["std"]["stdbw"]
+            Ximg += self.jsondata["mean"]["vmbw"]
+
+        if not yimg is None:
+            y1 = (yimg.transpose(1,3)[:,:,:,0] * self.jsondata["std"]["stdc0"])
+            y2 = (yimg.transpose(1,3)[:,:,:,1] * self.jsondata["std"]["stdc1"])
+            y3 = (yimg.transpose(1,3)[:,:,:,2] * self.jsondata["std"]["stdc2"])
+
+            y1 += self.jsondata["mean"]["vmc0"]
+            y2 += self.jsondata["mean"]["vmc1"]
+            y3 += self.jsondata["mean"]["vmc2"]
+
+        yimg = torch.stack([y1,y2,y3]).transpose(0,1)
+        #breakpoint()
+        return Ximg, yimg
+
+    def tensor_as_img(self, ten):
+        ten = ten.astype(np.uint8)
+        ten = cv.resize(ten, self.outsize)
+        ten[ten > 254] = 254
+        ten[ten < 0] = 0
+        return ten
+
+
     def __len__(self):
         return len(self.data)
     
@@ -84,12 +205,22 @@ class CustomDataset(Dataset):
         Ximg = cv2.resize(Ximg, self.insize)
         yimg = cv2.resize(yimg, self.outsize)
 
-        Ximg, yimg = self.normalize(Ximg, yimg)
+        # Ximg, yimg = self.normalize(Ximg, yimg)
 
-        Ximg_tensor = torch.tensor(np.array([Ximg]))
-        yimg_tensor = torch.tensor(yimg)
+        # Ximg_tensor = torch.tensor(Ximg).unsqueeze(0)
+        # yimg_tensor = torch.tensor(yimg)
+        # #breakpoint()
+        # yimg_tensor = yimg_tensor.permute(2, 1, 0)
 
-        Ximg_tensor = Ximg_tensor.permute(0, 2, 1)
-        yimg_tensor = yimg_tensor.permute(2, 1, 0)
+        xn = self.NBw(torch.tensor(Ximg).unsqueeze(0).float())
 
-        return Ximg_tensor, yimg_tensor
+        yn = self.NCol(torch.tensor(yimg).permute(2,0,1).float())
+
+        #uny = self.UNC2(yn)
+        #uxn = self.UnBw(xn)
+        # uyn = self.UnCol(yn)
+
+        # print(uyn.min(), " ", uyn.max())
+        #print(uxn.min(), " ", uxn.max())
+        #breakpoint()
+        return xn, yn
