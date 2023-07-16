aee69a92f12054de5135fd945c0ffd74357cf407
lollozip <lorenzo.orsingher@gmail.com>
2023-07-16 20:33:11 +0200

    add inference commands

--- utils.py
+++ utils.py
@@ -1,28 +1,9 @@
+import os
+import argparse
 import json
-import os
-import argparse
 from dotenv import load_dotenv
-def parse_argv():
-    load_dotenv()
-    parser=argparse.ArgumentParser()
-    parser.add_argument("--json", help="Path where json file will be saved/read", default=os.getenv('JSON_PATH'))
-    parser.add_argument("--data", help="Path to the dataset",default=os.getenv('DATA_PATH'))
-    parser.add_argument("--save", help="Path to where checkpoints will be saved",default=os.getenv('SAVE_PATH'))
-    parser.add_argument("--insize", help="Size of input image",default=32)
-    parser.add_argument("--outsize", help="Size of output image",default=32)
-    parser.add_argument("--batch", help="Size of batches",default=32)
-    parser.add_argument("--epochs", help="Number of epochs",default=300)
 
-    parser.add_argument("--video", help="Number of batches between every visualization",default=10)
-
-    parser.add_argument('--no-video', help = "Set for no visual debug", dest='video', action='store_false')
-
-    parser.add_argument("--load-chkp", help="Path to checkpoints to be loaded",dest='loadchkp',default="")
-
-import os
-import argparse
-from dotenv import load_dotenv
-def parse_argv():
+def parse_argv_train():
     load_dotenv()
     parser=argparse.ArgumentParser()
     parser.add_argument("--json", help="Path where json file will be saved/read", default=os.getenv('JSON_PATH'))
@@ -56,6 +37,28 @@ def parse_argv():
             ar.loadchkp,
             ar.colab)
 
+def parse_argv_inf():
+    load_dotenv()
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument("--input", help="Path to the image", default="")
+    parser.add_argument("--json", help="Path where json file will be saved/read", default=os.getenv('JSON_PATH'))
+    parser.add_argument("--save", help="Path to where checkpoints will be saved",default=os.getenv('SAVE_PATH'))
+    parser.add_argument("--size", help="Size of input image",default=32)
+
+    parser.add_argument("--load-chkp", help="Path to checkpoints to be loaded",dest='loadchkp',default="")
+    
+    parser.set_defaults(colab=False)
+    parser.add_argument('--colab', help = "Set for no visual debug", action='store_true')
+ 
+
+    ar=parser.parse_args()
+
+    return (ar.input,
+            ar.json,
+            (int(ar.size), int(ar.size)),
+            ar.loadchkp,
+            ar.colab)
 
 def save_loss(dict, path):
     with open(path + "loss.json", 'w') as fp:
